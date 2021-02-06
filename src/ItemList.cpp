16eaf83517aa0ce20f07dbf66242d11a43c57ca6
Rakesh Bandi <rbandi211570@gmail.com>
2021-02-05 23:27:51 -0800

    .

--- src/ItemList.cpp
+++ src/ItemList.cpp
@@ -13,6 +13,7 @@ public:
    array<double, MACROSCOUNT> sumNutri = {0,0,0,0,0,0};
    // variable length array storing name-item pairs
    ItemList() {}
+   // Memory Interactions
    void load(fstream& in){
       char str[256];
       while(in.getline(str,256)){
@@ -34,6 +35,7 @@ public:
          out << endl;
       }
    }
+   // Modifications
    void put(string a, array<double, MACROSCOUNT> vals){
       if(items.find(a)!=items.end()) this->remove(a);
       items.insert({a, Item(a, vals)});
@@ -50,11 +52,13 @@ public:
       items.clear();
       memset(&sumNutri, 0, sizeof(sumNutri));
    }
+   // Features
    array<double, MACROSCOUNT> average(int days){
       array<double, MACROSCOUNT> vals = sumNutri;
       for(int i=0;i<MACROSCOUNT;++i) vals[i]/=days;
       return vals;
    }
+   // Printing
    void print(fstream& out){
       for(std::pair<std::string,Item> elem : items){
          elem.second.print(out);
